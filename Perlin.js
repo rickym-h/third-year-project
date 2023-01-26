62c0a9657ecea957011f99284373c24c9c654d48
rickym-h <richardhull11@gmail.com>
2023-01-26 16:51:37 +0000

    updated to work with fractal octaves

--- Perlin.js
+++ Perlin.js
@@ -10,14 +10,15 @@ let perlin = {
     // Takes two points - the candidate point (x,y), and a grid corner (vx, vy)
     // Interpolates between the vector of the corner (grid_vector)
     // and the vector represented by corner->candidate point (candidate_vector)
-    dot_prod_grid: function(x, y, vx, vy){
+    dot_prod_grid: function(x, y, vx, vy, octave){
         let grid_vector;
         let candidate_vector = {x: x - vx, y: y - vy};
-        if (this.vector_map[[vx,vy]]){
-            grid_vector = this.vector_map[[vx,vy]];
+        let key = [vx, vy, octave].join(",");
+        if (this.vector_map.has(key)){
+            grid_vector = this.vector_map.get(key);
         } else {
             grid_vector = this.rand_vect();
-            this.vector_map[[vx, vy]] = grid_vector;
+            this.vector_map.set(key, grid_vector);
         }
         return candidate_vector.x * grid_vector.x + candidate_vector.y * grid_vector.y;
     },
@@ -36,32 +37,51 @@ let perlin = {
     // vector_map are saved vectors to use when calculating dot products
     // perlin_cache are saved perlin values so if the same coordinates are called, the same output is fetched
     seed: function(){
-        this.vector_map = {};
-        this.perlin_cache = {};
+        this.vector_map = new Map();
+        this.perlin_cache = new Map();
     },
 
     // Main function to get perlin noise
-    get: function(x, y) {
-        // Checks if the cache contains a value for this coordinate
-        if (this.perlin_cache.hasOwnProperty([x,y]))
-            return this.perlin_cache[[x,y]];
+    get: function(x, y, octave = 1) {
 
-        // Get the corners of the coordinate
-        let xf = Math.floor(x);
-        let yf = Math.floor(y);
-        // interpolate between these corners. (tl = top left, br = bottom right etc)
-        let tl = this.dot_prod_grid(x, y, xf,   yf);
-        let tr = this.dot_prod_grid(x, y, xf+1, yf);
-        let bl = this.dot_prod_grid(x, y, xf,   yf+1);
-        let br = this.dot_prod_grid(x, y, xf+1, yf+1);
+        let TotalPerlin = 0;
 
-        let xt = this.interpolate(x-xf, tl, tr);
-        let xb = this.interpolate(x-xf, bl, br);
-        let v = this.interpolate(y-yf, xt, xb);
+        x /= 2;
+        y /= 2;
 
-        // Saves to cache in case the same coord is called in the future
-        this.perlin_cache[[x,y]] = v;
-        return v;
+        for (let currentOctave = 1; currentOctave <= octave; currentOctave++) {
+
+            // adjust x and y to octaves
+            x *= 2;
+            y *= 2;
+
+            // Checks if the cache contains a value for this coordinate
+            let key = [x,y,currentOctave].join(",");
+            if (this.perlin_cache.has(key))
+                return this.perlin_cache.get(key);
+
+            // Get the corners of the coordinate
+            let xf = Math.floor(x);
+            let yf = Math.floor(y);
+            // interpolate between these corners. (tl = top left, br = bottom right etc)
+            let tl = this.dot_prod_grid(x, y, xf,   yf, currentOctave);
+            let tr = this.dot_prod_grid(x, y, xf+1, yf, currentOctave);
+            let bl = this.dot_prod_grid(x, y, xf,   yf+1, currentOctave);
+            let br = this.dot_prod_grid(x, y, xf+1, yf+1, currentOctave);
+
+            let xt = this.interpolate(x-xf, tl, tr);
+            let xb = this.interpolate(x-xf, bl, br);
+            let v = this.interpolate(y-yf, xt, xb);
+
+            // adjust based on octave amplitude
+            v /= (Math.pow(2, currentOctave-1));
+
+            // Saves to cache in case the same coord is called in the future
+            this.perlin_cache.set(key, v);
+            TotalPerlin += v;
+        }
+
+        return TotalPerlin;
     }
 }
 perlin.seed();
