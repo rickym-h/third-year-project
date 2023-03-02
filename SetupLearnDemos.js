30e3bd36e9235443baccc41b2a73d9e0fe57e4da
rickym-h <richardhull11@gmail.com>
2023-03-02 12:48:30 +0000

    added grid vector demo which generates a random set of 9 unit-length grid vectors

--- SetupLearnDemos.js
+++ SetupLearnDemos.js
@@ -124,3 +124,77 @@ button.addEventListener("click", () => {
     let y = 512 * Math.round(Math.random() * 100)/100
     GenerateCandidateVectorsAndUpdateDOM(x, y)
 })
+
+// Grid Vectors Demo
+
+let GVD_Canvas = document.getElementById("demo_GridVectors");
+let GVD_Context = GVD_Canvas.getContext('2d');
+
+function canvas_arrow(context, fromx, fromy, tox, toy) {
+    let headlen = 10; // length of head in pixels
+    let dx = tox - fromx;
+    let dy = toy - fromy;
+    let angle = Math.atan2(dy, dx);
+    context.beginPath()
+    context.moveTo(fromx, fromy);
+    context.lineTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+    context.moveTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+    context.stroke();
+}
+
+function GenerateRandomGridVectorsAndUpdateDOM() {
+    // Create Background
+    GVD_Context.fillStyle = "#808080";
+    GVD_Context.fillRect(0, 0, GVD_Canvas.width, GVD_Canvas.height)
+
+
+    // Draw Grid Lines
+    GVD_Context.beginPath();
+    GVD_Context.moveTo(1 * GVD_Canvas.width/4, 0);
+    GVD_Context.lineTo(1 * GVD_Canvas.width/4, GVD_Canvas.height);
+    GVD_Context.stroke();
+    GVD_Context.beginPath();
+    GVD_Context.moveTo(2 * GVD_Canvas.width/4, 0);
+    GVD_Context.lineTo(2 * GVD_Canvas.width/4, GVD_Canvas.height);
+    GVD_Context.stroke();
+    GVD_Context.beginPath();
+    GVD_Context.moveTo(3 * GVD_Canvas.width/4, 0);
+    GVD_Context.lineTo(3 * GVD_Canvas.width/4, GVD_Canvas.height);
+    GVD_Context.stroke();
+    GVD_Context.beginPath();
+    GVD_Context.moveTo(0, 1 * GVD_Canvas.width/4);
+    GVD_Context.lineTo(GVD_Canvas.width, 1 * GVD_Canvas.width/4);
+    GVD_Context.stroke();
+    GVD_Context.beginPath();
+    GVD_Context.moveTo(0, 2 * GVD_Canvas.width/4);
+    GVD_Context.lineTo(GVD_Canvas.width, 2 * GVD_Canvas.width/4);
+    GVD_Context.stroke();
+    GVD_Context.beginPath();
+    GVD_Context.moveTo(0, 3 * GVD_Canvas.width/4);
+    GVD_Context.lineTo(GVD_Canvas.width, 3 * GVD_Canvas.width/4);
+    GVD_Context.stroke();
+
+    for (let x = 128; x < 512; x+=128) {
+        for (let y = 128; y < 512; y+=128) {
+            // Calculate a random grid vector
+            let rand_vect = perlin.rand_vect();
+            let xEnd = x + (rand_vect.x * 128);
+            let yEnd = y + (rand_vect.y * 128);
+            console.log(xEnd, yEnd)
+
+            canvas_arrow(GVD_Context, x, y, xEnd, yEnd)
+        }
+    }
+
+
+
+
+}
+
+GenerateRandomGridVectorsAndUpdateDOM();
+button = document.getElementById("demo_GridVectors:NewGrid");
+button.addEventListener("click", () => {
+    GenerateRandomGridVectorsAndUpdateDOM();
+})
